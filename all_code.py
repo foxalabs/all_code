85a1ba98b624dd0d21d01cdf8bf940aa32e8f323
Kartik Mandar <kartik4321mandar@gmail.com>
2025-03-18 12:48:49 +0530

    add EXCLUDE_EXTENSIONS as global variable

--- all_code.py
+++ all_code.py
@@ -191,7 +191,7 @@ def main():
     args = parse_arguments()
 
     # Override the global options if command line arguments are provided.
-    global FULL_CODE_FILE_NAME, FILES_TO_INCLUDE, PROGRAMMING_EXTENSIONS, EXCLUDE_DIRS
+    global FULL_CODE_FILE_NAME, FILES_TO_INCLUDE, PROGRAMMING_EXTENSIONS, EXCLUDE_DIRS, EXCLUDE_EXTENSIONS
 
     if args.output_file:
         FULL_CODE_FILE_NAME = args.output_file
